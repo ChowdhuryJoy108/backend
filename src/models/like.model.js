b9eb021f2b0519880c02ff2e9042db10ea4ed970
Joy Chowdhury <imjoychy69@gmail.com>
2024-02-26 18:00:49 +0600

    models setup done

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,22 +1,25 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const likeSchema = new Schema({
-    video:{
-        type:Schema.Types.ObjectId,
-        ref:"video"
+const likeSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "video",
     },
-    comment:{
-        type:Schema.Types.ObjectId,
-        ref:"Comment"
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    tweet:{
-        type:Schema.Types.ObjectId,
-        ref:"Tweet"
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
     },
-    likeby:{
-        type:Schema.Types.ObjectId,
-        ref:"USer"
-    }
-},{timestamps:true})
+    likeby: {
+      type: Schema.Types.ObjectId,
+      ref: "USer",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Like = mongoose.model("Like", likeSchema)
+export const Like = mongoose.model("Like", likeSchema);
--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -24,5 +24,4 @@ const playlistSchema = new Schema(
   { timestamps: true }
 );
 
-
 export const Playlist = mongoose.model("Playlist", playlistSchema);
